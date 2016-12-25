9852b9d663660754ea7a12440a1e1428aca3b8ec
Vitthal-P <samrat.patil0202@gmail.com>
2016-12-26 00:23:28 +0530

    added chapter 7 practice project, simulate python strip() with regex

---
+++ regexStrip.py
@@ -0,0 +1,22 @@
+#/usr/bin/env python3
+#chapter 7 practice project-2
+
+import re
+
+rstripRegex = re.compile(r'^\s+')
+lstripRegex = re.compile(r'\s+$')
+
+def regStrip(text, char = None):
+    if char != None:
+        text = re.sub(char, '', text)
+    else:
+        newText = rstripRegex.sub('-', text)
+        text = lstripRegex.sub('-', newText)
+    print(text)
+
+print('Enter the text')
+userText = str(input())
+print('which character you want to strip : ')
+char = str(input())
+for text in userText.split(r'\n'):
+    regStrip(text, char)
