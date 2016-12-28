438fdf3d0c3c7c3b7be906e9bd086495adfb2c9d
Vitthal-P <samrat.patil0202@gmail.com>
2016-12-29 00:34:07 +0530

    madLib.py added all necessary loops and logic

--- madLib.py
+++ madLib.py
@@ -2,9 +2,33 @@
 # madLib.py - Replaces ADJECTIVE, NOUN, ADVERB, VERB with user input
 
 import re, os, sys
+
+
 def usage():
     print('Usage: madLib.py <file_name>')
 
-madLibRegex = re.compile(r'(ADJECTIVE|NOUN|ADVERB|VERB)')
+madLibRegex = re.compile(r'(ADJECTIVE)|(NOUN)|(ADVERB)|(VERB)')
 
+if len(sys.argv) == 2:
+    if os.path.isfile(sys.argv[1]):
+        inputFile = open(sys.argv[1])
+        outputFile = open('madLibOutput.txt', 'w')
+        for line in inputFile.readlines():
+            newLine = ''
+            for word in line.split():
+                mo = madLibRegex.search(word)
+                if mo:
+                    print('Enter {}'.format(mo.group()))
+                    replaceText = input()
+                    word = madLibRegex.sub(replaceText, word)
+                newLine += ' ' + word
+            outputFile.write(newLine)
+            print(newLine)
 
+        inputFile.close()
+        outputFile.close()
+    else:
+        print('please specify correct filename')
+        usage()
+else:
+    usage()
